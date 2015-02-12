be1db87fef85e227aea28ba5b005c4b6d7836291
Eric <eric.bgross@gmail.com>
2015-02-11 20:21:00 -0500

    Fixed gitignore workspace.xml problem

--- src/org/escaperun/game/model/items/Item.java
+++ src/org/escaperun/game/model/items/Item.java
@@ -1,7 +1,41 @@
 package org.escaperun.game.model.items;
 
+import org.escaperun.game.model.Activatable;
+import org.escaperun.game.model.Collidable;
+import org.escaperun.game.model.Drawable;
+import org.escaperun.game.model.entities.Entity;
+import org.escaperun.game.model.entities.Statistics;
+
 /**
  * Created by Eric on 2/11/2015.
  */
-public abstract class Item {
+public abstract class Item implements Drawable, Activatable, Collidable {
+    private final int[][] decal;
+    private Statistics stats;
+
+    public Item() {
+        this.decal = null;
+        this.stats = null;
+    }
+
+    public Item(int[][] decal, Statistics stats) {
+        this.decal = decal;
+        this.stats = stats;
+    }
+
+    public int[][] getDecal() {
+        return this.decal;
+    }
+
+    public Statistics getStats() {
+        return this.stats;
+    }
+
+    public boolean isCollidable() {
+        return false;
+    }
+
+    public abstract void onTouch(Entity e);
+
+    public abstract boolean isActivatable();
 }
