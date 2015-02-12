914dd77b494cdc60a2f8f9fc4b4dac73fd837e5d
mloaglbo <treefort2@gmail.com>
2015-02-11 21:29:49 -0500

    Added tile package. Added Terrain.java

---
+++ src/org/escaperun/game/model/tile/Terrain.java
@@ -0,0 +1,16 @@
+package org.escaperun.game.model.tile;
+
+import org.escaperun.game.model.Collidable;
+import org.escaperun.game.model.Drawable;
+
+/**
+ * Created by Matthew LoGalbo on 2/11/2015.
+ */
+public abstract class Terrain implements Collidable, Drawable {
+    int decal;
+    boolean collidable;
+
+    public int[][] getDecal(){
+        return new int[][] {{decal}};
+    }
+}
