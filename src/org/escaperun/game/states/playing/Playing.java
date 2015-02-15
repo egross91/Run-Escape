d58a2aed70d47de8001586e0a0fc9628e60e4cf0
aemery <agemery2@gmail.com>
2015-02-15 15:58:30 -0500

    Started work on the inventory window. Some progress made, not finished yet.

--- src/org/escaperun/game/states/playing/Playing.java
+++ src/org/escaperun/game/states/playing/Playing.java
@@ -72,45 +72,92 @@ public class Playing extends GameState {
 
 
     private Decal[][] createView(){
-        int statusRow, row, collumn, testRows = 0;
+        int statusRow, row, column, testRows = 0;
 
         Decal[][] gottenStage = stage.getRenderable();
 
         row = gottenStage.length;
-        collumn = gottenStage[0].length;
+        column = gottenStage[0].length;
         testRows = row;
 
         row = row + row%5;
         statusRow = row/5;
 
-        Decal[][] view = new Decal[statusRow + testRows][collumn];  // new 2D array with space for both viewports
+        Decal[][] view = new Decal[statusRow + testRows][column];  // new 2D array with space for both viewports
 
-        //if(stage.avatar.inv open)
         if(false){
             for(int x = 0; x < row; x++){
 
-                for(int y = 0; y < collumn; y++){
+                for(int y = 0; y < column; y++){
 
                 }
             }
         }
+        /*else if(stage.avatar.inv open) {
+            //here is where we would display an inventory, IF WE HAD ANY
+            //my thoughts are that there is going to be redundant code here & in the status view
+            //but I digress
+
+            //the edges of the window are gonna be '+'
+            Decal inventory_corner = new Decal('+', Color.GREEN, Color.BLACK);
+            view[testRows-statusRow][0] = inventory_corner;
+            view[testRows-statusRow][column-1] = inventory_corner;
+            view[testRows-1][0] = inventory_corner;
+            view[testRows-1][column-1] = inventory_corner;
+
+            //the rest of the border is '-'
+            Decal inventory_edge = new Decal('-', Color.GREEN, Color.BLACK);
+            for(int x = (testRows-statusRow); x < testRows; ++x){
+                for(int y = 0; y<column; ++y){
+                    if ((x == testRows-statusRow) && (y != 0) && (y != column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else if ((x == testRows-1) && (y != 0) && (y != column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else if ((y == 0) || (y == column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else
+                        view[x][y] = new Decal(';', Color.BLACK, Color.GREEN);
+                }
+            }
+        }*/
         //else show status viewport
         else{
             String stats = this.stage.getAvatar().getStats().toString();
             int col0, col1, col2,row0,row1,row2,row3,row4 = 0;
-            col0 = collumn/4;
+            col0 = column/4;
             col1 = 2*col0;
             col2 = 3*col0;
 
 
-            for(int xx = 0; xx < (testRows - statusRow); xx++){
-                for(int yy = 0; yy < collumn; yy++){
+            for(int xx = 0; xx < (testRows - statusRow); xx++){ //prob gonna display this stuff no matter what
+                for(int yy = 0; yy < column; yy++){
                     view[xx][yy] = gottenStage[xx][yy];
                 }
             }
-            for(int x3 = (testRows-statusRow); x3 < testRows; x3++){
-                for(int y3 = 0; y3<collumn; y3++){
-                    view[x3][y3] = new Decal('&', Color.RED, Color.BLACK);
+            Decal inventory_corner = new Decal('+', Color.GREEN, Color.BLACK);
+            view[testRows-statusRow][0] = inventory_corner;
+            view[testRows-statusRow][column-1] = inventory_corner;
+            view[testRows-1][0] = inventory_corner;
+            view[testRows-1][column-1] = inventory_corner;
+
+            //the rest of the border is '-'
+            Decal inventory_edge = new Decal('-', Color.GREEN, Color.BLACK);
+            for(int x = (testRows-statusRow); x < testRows; ++x){
+                for(int y = 0; y<column; ++y){
+                    if ((x == testRows-statusRow) && (y != 0) && (y != column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else if ((x == testRows-1) && (y != 0) && (y != column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else if ((y == 0) || (y == column-1)) {
+                        view[x][y] = inventory_edge;
+                    }
+                    else
+                        view[x][y] = new Decal(';', Color.BLACK, Color.GREEN);
                 }
             }
         }
