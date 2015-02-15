fa7bc5be0f04815c9a6a8838ca0375350291e048
aemery <agemery2@gmail.com>
2015-02-15 17:12:13 -0500

    added inventory key to keyboard

--- src/org/escaperun/game/Keyboard.java
+++ src/org/escaperun/game/Keyboard.java
@@ -15,6 +15,7 @@ public class Keyboard implements KeyListener {
     public static final char GRASS = 'g';
     public static final char ENTER = (char) 10;
     public static final char ESCAPE = (char) 27;
+    public static final char INVENTORY = 'i';
 
     public final boolean[] pressed = new boolean[65536]; // There are 2^16 = 65536 possible chars.
 
